16e16b62d4b9a8f93c2e3e484191c70e414fd2f0
Cristianps1988 <cristianps_1@hotmail.com>
2023-07-22 15:31:55 -0500

    navbar, sidebar added

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,6 +1,58 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from '../layouts/AuthLayout'
 
 export const RegisterPage = () => {
     return (
-        <div>RegisterPage</div>
+        <AuthLayout title='Login!'>
+            <form>
+                <Grid container>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Nombre Completo"
+                            type="text"
+                            placeholder="Jhon Doe"
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Correo"
+                            type="email"
+                            placeholder="correo@correo.com"
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Contraseña"
+                            type="password"
+                            placeholder="Password"
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+                        <Grid item xs={12}>
+                            <Button variant="contained" fullWidth>
+                                Crear Cuenta
+                            </Button>
+                        </Grid>
+
+                    </Grid>
+
+                    <Grid container direction="row" justifyContent="end">
+                        <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
+                        <Link component={RouterLink} color="inherit" to="/auth/login">
+                            Iniciar Sesión
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </AuthLayout>
+
     )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,14 @@
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
     return (
-        <div>JournalPage</div>
+        <JournalLayout>
+            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit magni debitis voluptate sint, officia facilis perferendis expedita quod inventore repellat, sapiente doloremque nihil dolorum eos, et repudiandae? Ratione, sed minus!</Typography>
+
+            {/* nothing selected */}
+            {/* journal */}
+
+        </JournalLayout>
     )
 }
