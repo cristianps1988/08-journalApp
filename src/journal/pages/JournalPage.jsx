4a60e02379728e705919f88f76b8e746a00786f8
Cristianps1988 <cristianps_1@hotmail.com>
2023-08-04 06:25:00 -0500

    start adding a new empty note

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,8 +3,17 @@ import { JournalLayout } from "../layout/JournalLayout"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 import { NoteView } from "../views/NoteView"
 import { AddOutlined } from "@mui/icons-material"
+import { useDispatch } from "react-redux"
+import { startNewNote } from "../../store/journalSlice/thunks"
 
 export const JournalPage = () => {
+
+    const dispatch = useDispatch()
+
+    const onClickNewNote = () => {
+        dispatch(startNewNote())
+    }
+
     return (
         <JournalLayout>
             {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit magni debitis voluptate sint, officia facilis perferendis expedita quod inventore repellat, sapiente doloremque nihil dolorum eos, et repudiandae? Ratione, sed minus!</Typography> */}
@@ -13,6 +22,7 @@ export const JournalPage = () => {
             {/* <NoteView /> */}
 
             <IconButton
+                onClick={onClickNewNote}
                 size="large"
                 sx={{
                     color: 'white',
